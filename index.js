5887b95ce361aafd4c2df0a78c8de47b534d9847
c0nfusecode <55307836+c0nfusecode@users.noreply.github.com>
2022-09-08 22:01:49 +0600

    update govno kode

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fileurl = '/pidoras'
 // /files/sharex/gmod_2022-09-03-201938.png
 
-app.get("/pidoras", (req, res) => {
+app.get("*/", (req, res) => {
 
   var ip = req.connection.remoteAddress.split(":")[3]
-  console.log(ip);
-  
-  res.send(`<html> <body> <img src="https://img.c0nfuse.ru/catjpg.jpg"> </body></html>`);
+
+  if(req.originalUrl == "/pidoras") return res.send(`<html> <body> <img src="https://img.c0nfuse.ru/catjpg.jpg"> </body></html>`);
+  res.send('вали нахуй от сюда!!!!'); 
+
 });
 
 app.listen(port, () => {
